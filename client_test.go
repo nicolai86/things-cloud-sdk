4d84b6f3f62c6db064017ffe1b98ce6589edd997
Raphael Randschau <nicolai86@me.com>
2017-05-25 07:30:00 +0200

    feat(sdk): add support to validate credentials

---
+++ client_test.go
@@ -0,0 +1,36 @@
+package thingscloud
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+)
+
+type fakeResponse struct {
+	statusCode int
+	file       string
+}
+
+func fakeServer(t fakeResponse) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open(fmt.Sprintf("tapes/%s", t.file))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Printf("Unable to open fixture %q path %q", t.file, r.URL.Path)
+			return
+		}
+		defer f.Close()
+
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Printf("Unable to load fixture %q path %q", t.file, r.URL.Path)
+			return
+		}
+		w.WriteHeader(t.statusCode)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, string(content))
+	}))
+}

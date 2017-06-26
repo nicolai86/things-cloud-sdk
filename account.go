3078f8e7baf4e898d49fff669a01e48167e26ca5
Raphael Randschau <nicolai86@me.com>
2017-06-26 22:34:14 +0200

    feat(sdk): add SignUp functionality
    to sign up, use a SDK client without username and password and call SignUp - it
    will return a new SDK client configured with the new account

--- account.go
+++ account.go
@@ -7,15 +7,45 @@ import (
 	"net/http"
 )
 
-type setPasswordBody struct {
-	Password string `json:"password"`
+type accountRequestBody struct {
+	Password           string `json:"password,omitempty"`
+	SLAVersionAccepted string `json:"SLA-version-accepted"`
+}
+
+// SignUp creates a new thingscloud account and returns a configured client
+func (c *Client) SignUp(email, password string) (*Client, error) {
+	data, err := json.Marshal(accountRequestBody{
+		Password:           password,
+		SLAVersionAccepted: "https://thingscloud.appspot.com/sla/v5.html",
+	})
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequest("PUT", fmt.Sprintf("/account/%s", email), bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	resp, err := c.do(req)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode == http.StatusUnauthorized {
+			return nil, ErrUnauthorized
+		}
+		return nil, fmt.Errorf("http response code: %s", resp.Status)
+	}
+
+	return New(c.Endpoint, email, password), nil
 }
 
 // SetAccountPassword allows you to change your account password.
 // Because things does not work with sessions you need to create a new client instance after
 // executing this method
 func (c *Client) SetAccountPassword(newPassword string) error {
-	data, err := json.Marshal(setPasswordBody{
+	data, err := json.Marshal(accountRequestBody{
 		Password: newPassword,
 	})
 	if err != nil {
